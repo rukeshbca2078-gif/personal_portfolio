a6dbb01df5f0308d364f80e1aea37cc28422522b
Rukesh Shrestha <rukeshbca2078@users.noreply.github.com>
2026-04-24 07:21:52 +0545

    first one

---
+++ portfolio/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Rukesh(models.Model):
+    title =models.CharField(max_length=100)
+    description= models.CharField(max_length=100)
+    image= models. ImageField(upload_to='portfolio/images/')
+    url= models.URLField(blank=True)
