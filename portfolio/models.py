e90e1dcd929386f60a7a26868ee4b63729a2f695
Unbounded <262159687+rukeshbca2078-gif@users.noreply.github.com>
2026-05-07 07:54:01 +0545

    Remove 'url' field from Rukesh model
    Removed the 'url' field from the Rukesh model.

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,4 +5,4 @@ class Rukesh(models.Model):
     title =models.CharField(max_length=100)
     description= models.CharField(max_length=100)
     image= models. ImageField(upload_to='portfolio/images/')
-    url= models.URLField(blank=True)
+    
